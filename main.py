455e6451206fdccb58441f8a463891380bbd23e8
Anish Jha <anishj2900@gmail.com>
2021-06-18 22:29:38 -0400

    More methods for plagiarism checker class

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ proposed by technological theorists fearful of
 artificial intelligence run rampant.
 '''
 
+conn = http.client.HTTPSConnection("google-search3.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "c4fb5f1f31msh492ecef4fd3350fp1d56b0jsn45ae19f27e93",
+    'x-rapidapi-host': "google-search3.p.rapidapi.com"
+}
+
 
 class PlagiarismChecker:
     def __init__(self, user_text):
@@ -21,27 +28,25 @@ class PlagiarismChecker:
         print(self.user_text)
         words = self.user_text.split(".")
         print(words)
+        conn.request("GET", "/api/v1/search/q=elon+musk&num=10", headers=headers)
+
+        res = conn.getresponse()
+        data = res.read()
+        print(data)
 
     def speaking_time(self):
         print(self.user_text)
 
+    def submit(self):
+        txt = t.get("1.0", "end")
+        self.user_text = txt
+        # self.checker()
 
-hi = PlagiarismChecker(text)
-hi.checker()
+    def get_text(self):
+        print(self.user_text)
 
-# uncomment this when you want to get search results
 
-# conn = http.client.HTTPSConnection("google-search3.p.rapidapi.com")
-#
-# headers = {
-#     'x-rapidapi-key': "c4fb5f1f31msh492ecef4fd3350fp1d56b0jsn45ae19f27e93",
-#     'x-rapidapi-host': "google-search3.p.rapidapi.com"
-#     }
-#
-# conn.request("GET", "/api/v1/search/q=elon+musk&num=10", headers=headers)
-#
-# res = conn.getresponse()
-# data = res.read()
+hi = PlagiarismChecker('')
 
 window = tk.Tk()
 label = tk.Label(text="Topic", bg='black', fg='#fff')
@@ -51,8 +56,10 @@ entry = tk.Entry()
 frame = tk.Frame(window, width=400, height=20, background='black')
 frame1 = tk.Frame(window, width=400, height=20, background='black')
 frame2 = tk.Frame(window, width=300, height=20, background='black')
+frame3 = tk.Frame(window, width=300, height=20, background='black')
 
 t = tk.Text(window, height=20, width=45)
+button = tk.Button(window, text="Submit", width=10, command=hi.submit)
 window.configure(background='black')
 
 frame.pack(fill=None, expand=False)
@@ -62,6 +69,9 @@ frame1.pack(fill=None, expand=False)
 label1.pack()
 t.pack()
 frame2.pack(fill=None, expand=False)
+button.pack()
+frame3.pack(fill=None, expand=False)
+
 
 window.mainloop()
 
